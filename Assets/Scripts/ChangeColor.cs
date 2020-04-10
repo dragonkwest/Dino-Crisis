9a573a4e10bb5d4c4b94ba527203f7d560066455
dragonkwest <61054616+dragonkwest@users.noreply.github.com>
2020-04-10 14:16:36 -0400

    More Scripts

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private Renderer rend;
+    [SerializeField]
+    private Color colorToTurnTo = Color.white;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        rend.material.color = colorToTurnTo;
+    }
+
+
+}
---
+++ Assets/Scripts/ShopControlScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopControlScript : MonoBehaviour
+{
+    int coinCount;
+    int isSkinSold;
+
+    public Text coinText;
+    public Text skinPrice;
+
+    public Button buyButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCount = PlayerPrefs.GetInt ("coinCount");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = "Coins: " + coinCount.ToString() + "$";
+
+        isSkinSold = PlayerPrefs.GetInt("isSkinSold");
+
+        if (coinCount >= 1 && isSkinSold == 0)
+            buyButton.interactable = true;
+        else
+            buyButton.interactable = false;
+    }
+
+    public void buySkin()
+    {
+        coinCount -= 1;
+        PlayerPrefs.SetInt("isSkinSold", 1);
+        skinPrice.text = "Sold!";
+        buyButton.gameObject.SetActive(false);
+    }
+    }
+}
