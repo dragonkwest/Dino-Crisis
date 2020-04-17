fc4842c437c8ac385ae1d18648b586cc8978284b
dragonkwest <61054616+dragonkwest@users.noreply.github.com>
2020-04-17 15:07:44 -0400

    The ground works!

--- Assets/Scripts/CameraController1.cs
+++ Assets/Scripts/CameraController1.cs
@@ -29,12 +29,12 @@ public class CameraController1 : MonoBehaviour {
         if (followTarget) 
 		{
 			
-			targetPosition = new Vector3 (target.transform.position.x, transform.position.y, transform.position.z);
+			targetPosition = new Vector3 (target.transform.position.x, target.transform.position.y, transform.position.z);
 
 			if (target.transform.localScale.x > 0f) {
-				targetPosition = new Vector3 (targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
-			} else {
 				targetPosition = new Vector3 (targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+			} else {
+				targetPosition = new Vector3 (targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
 			}
 
 			//transform.position = targetPosition;
